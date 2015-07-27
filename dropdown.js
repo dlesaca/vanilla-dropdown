e7d7a0dd4e4c10d820bba37859a00fcc449a07fa
Dexter Lesaca <emobile@gmail.com>
2015-07-27 12:36:19 -0700

    initial commit

---
+++ dropdown.js
@@ -0,0 +1,27 @@
+(function(){
+	var trigger = document.getElementById('trigger');
+	var menu = document.getElementById('menu');
+	var menuItem = document.querySelectorAll('li.menu__item');
+
+	trigger.addEventListener('mouseover', function(){
+		menu.classList.remove('collapsed');
+	}, false);
+
+	trigger.addEventListener('mouseout', function(){
+		menu.classList.add('collapsed');
+	}, false);
+
+	for (var i = 0; i < menuItem.length; i++) {
+		if (menuItem[i].querySelector('ul.drop-menu--sub')) {
+			(function(i) {
+				menuItem[i].addEventListener('mouseover', function() {
+					menuItem[i].querySelector('ul.drop-menu--sub').classList.remove('collapsed')
+				}, false);
+
+				menuItem[i].addEventListener('mouseout', function() {
+					menuItem[i].querySelector('ul.drop-menu--sub').classList.add('collapsed');
+				}, false);
+			})(i);
+		}
+	}
+})();
